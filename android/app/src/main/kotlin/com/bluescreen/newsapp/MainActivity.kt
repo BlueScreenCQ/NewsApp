4bdadc333524056963e948e82260c34fdb3aa6b2
BlueScreen <fran_castro_quintana@outlook.com>
2021-05-13 13:32:26 +0200

    Inicio del proyecto

---
+++ android/app/src/main/kotlin/com/bluescreen/newsapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bluescreen.newsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/tabs_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Tabs page'),
+      ),
+    );
+  }
+}
